74ca150d40becaeb12020a4779c89273e1aaf3ee
Josh Pencheon <josh.pencheon@googlemail.com>
2016-03-02 22:53:11 +0000

    # use PhantomJS for integration testing

--- test/dummy/app/controllers/disaster_controller.rb
+++ test/dummy/app/controllers/disaster_controller.rb
@@ -1,5 +1,8 @@
 # Some application logic that we should be able to log failures from:
 class DisasterController < ApplicationController
+  def no_panic
+  end
+
   def cause
     fail params[:message]
   end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   mount NdrError::Engine => '/fingerprinting'
 
   get '/:controller(/:action(/:id))'
+
+  root to: 'disaster#no_panic'
 end
