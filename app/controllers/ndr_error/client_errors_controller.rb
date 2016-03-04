637df7ef5c6ac9f5683146868d4fc10f5f82fae7
Josh Pencheon <josh.pencheon@googlemail.com>
2016-03-04 09:48:55 +0000

    more theory

--- app/controllers/ndr_error/client_errors_controller.rb
+++ app/controllers/ndr_error/client_errors_controller.rb
@@ -2,20 +2,22 @@ module NdrError
   # Controller for receiving client errors
   class ClientErrorsController < ApplicationController
     def create
-      Rails.logger.info(params.inspect)
+      exception = JavascriptError.new(params[:client_error])
 
-      exception  = Exception.new(params[:message])
-      parameters = {}
+      error_params = params[:client_error]
+      exception    = Exception.new(error_params.delete(:message))
+
 
       # TODO: this needs to be a separate message
-      fingerprint, log = NdrError.log(exception, parameters, request)
+      fingerprint, log = NdrError.log(exception, {}, request)
 
       # TODO: set error_class / message separately
       # TODO: strip 
       # TODO: don't do this here!
       log.update_attributes(
-        url: params[:path],
-        backtrace: (params[:stack] || '').split("\n")
+        url: request.env['HTTP_REFERER'],
+        backtrace: (error_params.delete(:stack) || '').split("\n"),
+        parameters_yml: error_params
       )
 
       render json: {
