c3fc2bf93bb94b2584ece20c6b545a542a592091
Josh Pencheon <josh.pencheon@googlemail.com>
2016-06-30 15:29:03 +0100

    # update testing schema dump

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,17 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20150918163720) do
 
-  create_table "error_fingerprints", id: false, force: true do |t|
-    t.string   "error_fingerprintid"
+  create_table "error_fingerprints", primary_key: "error_fingerprintid", id: :string, force: :cascade do |t|
     t.string   "ticket_url"
     t.string   "status"
     t.integer  "count"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["error_fingerprintid"], name: "sqlite_autoindex_error_fingerprints_1", unique: true
   end
 
-  create_table "error_logs", id: false, force: true do |t|
-    t.string   "error_logid"
+  create_table "error_logs", primary_key: "error_logid", id: :string, force: :cascade do |t|
     t.string   "error_fingerprintid"
     t.string   "error_class"
     t.text     "description"
@@ -43,6 +41,8 @@ ActiveRecord::Schema.define(version: 20150918163720) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "custom_user_column"
+    t.index ["error_fingerprintid"], name: "index_error_logs_on_error_fingerprintid"
+    t.index ["error_logid"], name: "sqlite_autoindex_error_logs_1", unique: true
   end
 
 end
